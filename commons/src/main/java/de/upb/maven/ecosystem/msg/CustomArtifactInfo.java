874d08b0154bf8b5a1f4123b96556d461bcbf8d4
Andreas Dann <adann@mail.uni-paderborn.de>
2022-01-11 17:19:33 +0100

    add commons code for projects

---
+++ commons/src/main/java/de/upb/maven/ecosystem/msg/CustomArtifactInfo.java
@@ -0,0 +1,27 @@
+package de.upb.maven.ecosystem.msg;
+
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@JsonIgnoreProperties(ignoreUnknown = true)
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class CustomArtifactInfo {
+
+  private String classifier;
+  private String groupId;
+  private String artifactVersion;
+  private String fileExtension;
+  private String artifactId;
+  private String distribution;
+  private String licenseUrl;
+  private String bundleLicense;
+  private String repoURL;
+
+}
