d9d2d894cba1b2d1c3bedde0a94d775b0070dbbc
Andreas Dann <adann@mail.uni-paderborn.de>
2022-01-12 09:55:56 +0100

    add Dao Pattern for Neo4J Database

---
+++ commons/src/main/java/de/upb/maven/ecosystem/persistence/Dao.java
@@ -0,0 +1,19 @@
+package de.upb.maven.ecosystem.persistence;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface Dao<T> {
+
+    Optional<T> get(long id);
+
+    Optional<T> get(T instance);
+
+    List<T> getAll();
+
+    void save(T t);
+
+    void update(T t, String[] params);
+
+    void delete(T t);
+}
---
+++ commons/src/main/java/de/upb/maven/ecosystem/persistence/DaoMvnArtifactNode.java
@@ -0,0 +1,11 @@
+package de.upb.maven.ecosystem.persistence;
+
+import java.util.Optional;
+
+public interface DaoMvnArtifactNode extends Dao<MvnArtifactNode> {
+
+    Optional<MvnArtifactNode> getParent(long id);
+
+    Optional<MvnArtifactNode> getParent(MvnArtifactNode instance);
+
+}
