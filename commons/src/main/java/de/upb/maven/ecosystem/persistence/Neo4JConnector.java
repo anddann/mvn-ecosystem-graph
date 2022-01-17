242ea54ca6ab86a3b786fe19ff4e080b18084a34
Andreas Dann <adann@mail.uni-paderborn.de>
2022-01-17 17:22:34 +0100

    fix code

--- commons/src/main/java/de/upb/maven/ecosystem/persistence/Neo4JConnector.java
+++ commons/src/main/java/de/upb/maven/ecosystem/persistence/Neo4JConnector.java
@@ -5,36 +5,44 @@ import org.neo4j.driver.Driver;
 import org.neo4j.driver.GraphDatabase;
 
 public class Neo4JConnector {
-  private static Driver instance;
+    private static Driver instance;
 
-  public static String getCrawlerVersion() {
-    String res = System.getenv("CRAWLER_VERSION");
-    if (res == null || res.isEmpty()) {
-      res = "0.5.0";
+    public static String getCrawlerVersion() {
+        String res = System.getenv("CRAWLER_VERSION");
+        if (res == null || res.isEmpty()) {
+            res = "0.5.0";
+        }
+        return res;
     }
-    return res;
-  }
 
-  public static String getNeo4jURL() {
-    String res = System.getenv("NEO4J_URL");
-    if (res == null || res.isEmpty()) {
-      res = "bolt://localhost:7687";
+    public static String getNeo4jURL() {
+        String res = System.getenv("NEO4J_URL");
+        if (res == null || res.isEmpty()) {
+            res = "bolt://localhost:7687";
+        }
+        return res;
     }
-    return res;
-  }
 
-  public static String getNeo4jPASS() {
-    String res = System.getenv("NEO4J_PASS");
-    if (res == null || res.isEmpty()) {
-      res = "test";
+    public static String getNeo4jUser() {
+        String res = System.getenv("NEO4J_USER");
+        if (res == null || res.isEmpty()) {
+            res = "neo4j";
+        }
+        return res;
     }
-    return res;
-  }
 
-  public static Driver getDriver() {
-    if (instance == null) {
-      instance = GraphDatabase.driver(getNeo4jURL(), AuthTokens.basic("neo4j", getNeo4jPASS()));
+    public static String getNeo4jPASS() {
+        String res = System.getenv("NEO4J_PASS");
+        if (res == null || res.isEmpty()) {
+            res = "test";
+        }
+        return res;
     }
-    return instance;
-  }
+
+    public static Driver getDriver() {
+        if (instance == null) {
+            instance = GraphDatabase.driver(getNeo4jURL(), AuthTokens.basic(getNeo4jUser(), getNeo4jPASS()));
+        }
+        return instance;
+    }
 }
