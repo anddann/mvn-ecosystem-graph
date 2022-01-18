de1ebe74ba38339a84b949fdfc58e6e3d413ce19
Andreas Dann <adann@mail.uni-paderborn.de>
2022-01-18 10:21:15 +0100

    update test case

--- mvn-crawler/src/test/java/de/upb/maven/ecosystem/crawler/process/ArtifactProcessorTest.java
+++ mvn-crawler/src/test/java/de/upb/maven/ecosystem/crawler/process/ArtifactProcessorTest.java
@@ -4,86 +4,93 @@ import de.upb.maven.ecosystem.msg.CustomArtifactInfo;
 import de.upb.maven.ecosystem.persistence.DaoMvnArtifactNode;
 import de.upb.maven.ecosystem.persistence.DoaMvnArtifactNodeImpl;
 import de.upb.maven.ecosystem.persistence.MvnArtifactNode;
+import junit.framework.TestCase;
+
 import java.io.IOException;
+import java.util.Collection;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Optional;
-import junit.framework.TestCase;
 
 public class ArtifactProcessorTest extends TestCase {
 
-  private final DaoMvnArtifactNode databaseMock =
-      new DaoMvnArtifactNode() {
-        // the database
-        private final HashMap<String, MvnArtifactNode> cache = new HashMap<>();
+    private final DaoMvnArtifactNode databaseMock =
+            new DaoMvnArtifactNode() {
+                // the database
+                private final HashMap<String, MvnArtifactNode> cache = new HashMap<>();
 
-        @Override
-        public Optional<MvnArtifactNode> getParent(long id) {
-          return Optional.empty();
-        }
+                @Override
+                public Optional<MvnArtifactNode> getParent(long id) {
+                    return Optional.empty();
+                }
 
-        @Override
-        public Optional<MvnArtifactNode> getParent(MvnArtifactNode instance) {
-          return Optional.empty();
-        }
+                @Override
+                public Optional<MvnArtifactNode> getParent(MvnArtifactNode instance) {
+                    return Optional.empty();
+                }
 
-        @Override
-        public boolean containsNodeWithVersionGQ(
-            String groupId,
-            String artifactId,
-            String version,
-            String classifier,
-            String targetVersion) {
-          return false;
-        }
+                @Override
+                public boolean containsNodeWithVersionGQ(
+                        String groupId,
+                        String artifactId,
+                        String version,
+                        String classifier,
+                        String targetVersion) {
+                    return false;
+                }
 
-        @Override
-        public Optional<MvnArtifactNode> get(long id) {
-          return Optional.empty();
-        }
+                @Override
+                public Optional<MvnArtifactNode> get(long id) {
+                    return Optional.empty();
+                }
 
-        @Override
-        public Optional<MvnArtifactNode> get(MvnArtifactNode instance) {
+                @Override
+                public Optional<MvnArtifactNode> get(MvnArtifactNode instance) {
 
-          return Optional.ofNullable(cache.get(instance.getGroup() + instance.getArtifact()));
-        }
+                    return Optional.ofNullable(cache.get(instance.getGroup() + instance.getArtifact()));
+                }
 
-        @Override
-        public List<MvnArtifactNode> getAll() {
-          return null;
-        }
+                @Override
+                public List<MvnArtifactNode> getAll() {
+                    return null;
+                }
 
-        @Override
-        public void save(MvnArtifactNode mvnArtifactNode) {}
+                @Override
+                public void save(MvnArtifactNode mvnArtifactNode) {
+                }
 
-        @Override
-        public void saveOrMerge(MvnArtifactNode instance) {
-          DoaMvnArtifactNodeImpl.sanityCheck(instance);
-          cache.put(instance.getGroup() + instance.getArtifact(), instance);
-        }
+                @Override
+                public void saveOrMerge(MvnArtifactNode instance) {
+                    DoaMvnArtifactNodeImpl.sanityCheck(instance);
+                    cache.put(instance.getGroup() + instance.getArtifact(), instance);
+                }
 
-        @Override
-        public void update(MvnArtifactNode mvnArtifactNode, String[] params) {}
+                @Override
+                public void update(MvnArtifactNode mvnArtifactNode, String[] params) {
+                }
 
-        @Override
-        public void delete(MvnArtifactNode mvnArtifactNode) {}
-      };
+                @Override
+                public void delete(MvnArtifactNode mvnArtifactNode) {
+                }
+            };
 
-  public void testProcess() throws IOException {
+    public void testProcess() throws IOException {
 
-    ArtifactProcessor artifactProcessor =
-        new ArtifactProcessor(databaseMock, "https://repo1.maven.org/maven2/");
+        ArtifactProcessor artifactProcessor =
+                new ArtifactProcessor(databaseMock, "https://repo1.maven.org/maven2/");
 
-    CustomArtifactInfo artifactInfo = new CustomArtifactInfo();
-    artifactInfo.setRepoURL("https://repo1.maven.org/maven2/");
-    artifactInfo.setGroupId("io.atlasmap");
-    artifactInfo.setArtifactId("atlas-csv-service");
-    artifactInfo.setArtifactVersion("2.2.0-M.3");
-    artifactInfo.setFileExtension("jar");
-    artifactInfo.setPackaging("jar");
+        CustomArtifactInfo artifactInfo = new CustomArtifactInfo();
+        artifactInfo.setRepoURL("https://repo1.maven.org/maven2/");
+        artifactInfo.setGroupId("io.atlasmap");
+        artifactInfo.setArtifactId("atlas-csv-service");
+        artifactInfo.setArtifactVersion("2.2.0-M.3");
+        artifactInfo.setFileExtension("jar");
+        artifactInfo.setPackaging("jar");
 
-    final MvnArtifactNode process = artifactProcessor.process(artifactInfo);
+        final Collection<MvnArtifactNode> process = artifactProcessor.process(artifactInfo);
 
-    assertNotNull(process);
-  }
+        assertNotNull(process);
+        assertFalse(process.isEmpty());
+        //TODO add semantic test
+    }
 }
