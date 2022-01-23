34782be7693c5d9603da4a6625039ed2d8aecab7
Andreas Dann <adann@mail.uni-paderborn.de>
2022-01-23 11:06:13 +0100

    fix npe

--- commons/src/main/java/de/upb/maven/ecosystem/persistence/dao/DoaMvnArtifactNodeImpl.java
+++ commons/src/main/java/de/upb/maven/ecosystem/persistence/dao/DoaMvnArtifactNodeImpl.java
@@ -8,17 +8,6 @@ import com.google.common.base.Stopwatch;
 import de.upb.maven.ecosystem.persistence.model.DependencyRelation;
 import de.upb.maven.ecosystem.persistence.model.DependencyScope;
 import de.upb.maven.ecosystem.persistence.model.MvnArtifactNode;
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.Iterator;
-import java.util.List;
-import java.util.Map;
-import java.util.Set;
-import java.util.function.BiFunction;
-import java.util.function.Function;
-import java.util.stream.Collectors;
 import org.apache.commons.lang3.StringUtils;
 import org.apache.commons.lang3.tuple.Pair;
 import org.jetbrains.annotations.NotNull;
@@ -33,6 +22,18 @@ import org.neo4j.driver.Value;
 import org.neo4j.driver.exceptions.ClientException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.function.BiFunction;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 
 public class DoaMvnArtifactNodeImpl implements DaoMvnArtifactNode {
 
@@ -704,6 +705,11 @@ public class DoaMvnArtifactNodeImpl implements DaoMvnArtifactNode {
                 }
               });
     }
+    if (versionNumberAndResolvingLevel == null) {
+      // we did not find a node
+      return false;
+    }
+
     String versionNumber = versionNumberAndResolvingLevel.getLeft();
     String resolvingLevel = versionNumberAndResolvingLevel.getRight();
     if (StringUtils.isBlank(versionNumber) || StringUtils.isBlank(resolvingLevel)) {
