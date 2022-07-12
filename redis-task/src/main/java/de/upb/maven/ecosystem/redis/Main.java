b8d24c88c13596ca60d7ed75dad640b2dd932cd2
Andreas Dann <adann@mail.uni-paderborn.de>
2022-07-12 10:53:59 +0200

    add sleep time until redis is up and running

--- redis-task/src/main/java/de/upb/maven/ecosystem/redis/Main.java
+++ redis-task/src/main/java/de/upb/maven/ecosystem/redis/Main.java
@@ -2,9 +2,31 @@ package de.upb.maven.ecosystem.redis;
 
 import de.upb.maven.ecosystem.persistence.redis.RedisSerializerUtil;
 
+import java.io.IOException;
+import java.net.Socket;
+
 public class Main {
 
-  public static void main(String[] args) {
+  public static void main(String[] args) throws InterruptedException {
+
+    System.out.println("Check if redis is up and running");
+    boolean redisAvialable = false;
+    while (!redisAvialable) {
+      redisAvialable = reachable();
+      if (!redisAvialable) {
+        System.out.println("redis is not available waiting for sec: " + 30);
+        Thread.sleep(1000 * 30);
+      }
+    }
+
     Redis2Neo4JDB.init(RedisSerializerUtil.getRedisURLFromEnvironment());
+  }
+
+  private static boolean reachable() {
+    try (Socket ignored = new Socket(RedisSerializerUtil.getRedisURLFromEnvironment(), 6379)) {
+      return true;
+    } catch (IOException ignored) {
+      return false;
+    }
   }
 }
